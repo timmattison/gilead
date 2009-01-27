1cca8eef8bd3bb92874a6e1967e7aeba1714e8d7
bmarchesson <bmarchesson@8cb42d02-3994-4270-bb0b-85ab9ff68958>
2009-01-27 20:52:36 +0000

    GWT test case for performance improvement
    git-svn-id: https://gilead.svn.sourceforge.net/svnroot/gilead/gilead/trunk@264 8cb42d02-3994-4270-bb0b-85ab9ff68958

--- adapter-test/src/net/sf/gilead/performance/gwt/GwtPerformanceTest.java
+++ adapter-test/src/net/sf/gilead/performance/gwt/GwtPerformanceTest.java
@@ -3,9 +3,13 @@
  */
 package net.sf.gilead.performance.gwt;
 
-import com.google.gwt.junit.client.GWTTestCase;
+import net.sf.gilead.test.domain.interfaces.IUser;
 
-import junit.framework.TestCase;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
 /**
  * Performance test for GWT
@@ -37,7 +41,48 @@ public class GwtPerformanceTest extends GWTTestCase
 	 */
 	public void testCloneUserAndMessage()
 	{
-		assertTrue(true);
+		// Setup an asynchronous event handler.
+		Timer timer = new Timer()
+		{
+			public void run()
+			{
+				// Call performance service
+				PerformanceServiceAsync remoteService = (PerformanceServiceAsync) GWT.create(PerformanceService.class);
+				((ServiceDefTarget) remoteService).setServiceEntryPoint( GWT.getModuleBaseURL() + "/PerformanceService");
+				
+				final long start = System.currentTimeMillis();
+				remoteService.loadUserAndMessages(new AsyncCallback<IUser>()
+				{
+					public void onFailure(Throwable caught)
+					{
+						assertFalse(caught.toString(), false);
+
+						// tell the test system the test is now done
+						finishTest();
+					}
+
+					public void onSuccess(IUser result)
+					{
+						long end = System.currentTimeMillis();
+						assertNotNull(result);
+						
+						System.out.println("Received user after " + (end - start) + " ms.");
+						
+						// tell the test system the test is now done
+						finishTest();
+					}
+			
+				});
+
+			}
+		};
+
+		// Set a delay period significantly longer than the
+		// event is expected to take.
+		delayTestFinish(5000);
+
+		// Schedule the event and return control to the test system.
+		timer.schedule(100);
 	}
 
 }
---
+++ adapter-test/src/net/sf/gilead/performance/gwt/PerformanceTestAsync.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package net.sf.gilead.performance.gwt;
+
+/**
+ * @author bruno.marchesson
+ * @generated generated asynchronous callback interface to be used on the client side
+ *
+ */
+public interface PerformanceTestAsync {
+
+}
