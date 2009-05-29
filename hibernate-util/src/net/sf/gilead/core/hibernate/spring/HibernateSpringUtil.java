d04fc715556a7362fafcc71d8e278cc1be0b85a8
bmarchesson <bmarchesson@8cb42d02-3994-4270-bb0b-85ab9ff68958>
2009-05-29 20:44:32 +0000

    Spring sessionFactory handling improvement
    git-svn-id: https://gilead.svn.sourceforge.net/svnroot/gilead/gilead/branches/1.2@892 8cb42d02-3994-4270-bb0b-85ab9ff68958

--- hibernate-util/src/net/sf/gilead/core/hibernate/spring/HibernateSpringUtil.java
+++ hibernate-util/src/net/sf/gilead/core/hibernate/spring/HibernateSpringUtil.java
@@ -3,11 +3,17 @@
  */
 package net.sf.gilead.core.hibernate.spring;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
 import net.sf.gilead.core.hibernate.HibernateUtil;
+import net.sf.gilead.util.IntrospectionHelper;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.ejb.HibernateEntityManagerFactory;
+import org.hibernate.impl.SessionFactoryImpl;
 import org.springframework.aop.framework.AopProxy;
+import org.springframework.aop.support.AopUtils;
 
 /**
  * Spring handling of injected session factories
@@ -29,9 +35,17 @@ public class HibernateSpringUtil extends HibernateUtil
 		//	Need to call 'getProxy' method
 		//
 			entityManagerFactory = ((AopProxy) entityManagerFactory).getProxy();
-		
 		}
+		else if (AopUtils.isJdkDynamicProxy(entityManagerFactory))
+		{
+		//	JDK dynamic proxy
+		//
+			InvocationHandler invocationHandler = Proxy.getInvocationHandler(entityManagerFactory); 
+			entityManagerFactory = (SessionFactory) IntrospectionHelper.searchMember(HibernateEntityManagerFactory.class, 
+																					invocationHandler); 
+		} 
 		
+
 	//	Just get session factory for Hibernate emf implementation
 	//
 		if (entityManagerFactory instanceof HibernateEntityManagerFactory)
@@ -56,8 +70,14 @@ public class HibernateSpringUtil extends HibernateUtil
 		//	Need to call 'getProxy' method
 		//
 			sessionFactory = (SessionFactory) ((AopProxy) sessionFactory).getProxy();
-		
 		}
+		else if (AopUtils.isJdkDynamicProxy(sessionFactory))
+		{
+		//	JDK dynamic proxy
+		//
+			InvocationHandler invocationHandler = Proxy.getInvocationHandler(sessionFactory); 
+			sessionFactory = (SessionFactory) IntrospectionHelper.searchMember(SessionFactoryImpl.class, invocationHandler); 
+		} 
 		
 		// Call base class
 		super.setSessionFactory(sessionFactory);
